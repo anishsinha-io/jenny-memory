b02d3b2c8b9f6bd698bcb4c725b9df20394ead22
Anish Sinha <anishsinha0128@gmail.com>
2023-09-29 23:06:41 -0400

    Create realloc example

--- main.c
+++ main.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stddef.h>
 
 int main() {
-    // Example of dereferencing a null pointer. 0x0 is never a memory address we own
-    *(uintptr_t *) (0x0) = 1;
-
-    // Will also (probably) segfault (the process we are running this program in
-    // likely does not own this random address
-    *(uintptr_t *) (0x69206C6) = 2;
-
-    // We have an incomplete type. We cannot dereference an incomplete type.
-    // This is NOT a null pointer error because the address returned by malloc is guaranteed
-    // to be in our process's segmented memory
-    void *z = malloc(50);
-    printf("%d\n", *z);
-
+    void *mem = malloc(100);
+    printf("%p\n", mem);
+    mem = realloc(mem, 120);
+    printf("%p\n", mem);
     return 0;
 }
