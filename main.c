a381b5108189cdfadcecb2ac8041a0c70584b7f7
Anish Sinha <anishsinha0128@gmail.com>
2023-10-05 16:11:57 -0400

    Create constructors code

--- main.c
+++ main.c
@@ -1,20 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int fn() { return 5; }
-double fn2() { return 5.0; }
+struct person {
+    char* first_name;
+    char* last_name;
+    char* race;
+    char* gender;
+    unsigned int age;
+};
 
-void fn3() {}
+struct person* new_person(char* first_name, char* last_name, char* race,
+                          char* gender, unsigned int age) {
+    struct person* p = malloc(sizeof(struct person));
+    p->first_name = first_name;
+    p->last_name = last_name;
+    p->age = age;
+    p->race = race;
+    p->gender = gender;
+    return p;
+}
 
-int* fn4() {
-    int* x = malloc(sizeof(int));
-    *x = 5;
-    return x;
+void delete_person(struct person* p) {
+    free(p->first_name);
+    free(p->last_name);
+    free(p->race);
+    free(p->gender);
+    free(p);
 }
 
 int main() {
-    int* res = fn4();
-    printf("%d\n", *res);
-    free(res);
+    struct person* jenny = new_person("jenny", "sinha", "korean", "female", 21);
+    delete_person(jenny);
     return 0;
 }
