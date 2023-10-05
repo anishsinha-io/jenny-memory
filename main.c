0f5a4c85895382b0ef8b537b024ee8e6a4b47e87
Anish Sinha <anishsinha0128@gmail.com>
2023-10-05 15:26:06 -0400

    Create example of returning pointer to heap with malloc

--- main.c
+++ main.c
@@ -1,37 +1,14 @@
-#include <fcntl.h>
-#include <stddef.h>
-#include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    // Allocate 100 bytes
-    void* mem = malloc(100);
+int fn() { return 5; }
+double fn2() { return 5.0; }
 
-    // Print the address of mem
-    printf("%p\n", mem);
+void fn3() {}
 
-    // Reallocates (elongates) our allocated area of memory from 100 bytes to 120 bytes
-    mem = realloc(mem, 120);
-
-    // Print the address of mem
-    printf("%p\n", mem);
-
-    // Print the address of mem increment by 1 because on this system, void is sized as 1
-    printf("%p\n", mem + 1);
-
-    printf("%p\n", mem + 2);
-
-    // Casting mem to be an int pointer
-    int* casted = mem;
-
-    // Prints the value at "casted" (which is the first 4 bytes of mem interpreted as an integer)
-    printf("%d\n", *casted);
-
-    // The address at mem + 12 is casted as an integer pointer and the value at that address is set to 8
-    *(int*)(mem + 12) = 8;
-
-    // Void isn't typed, we need to cast it. We cast the address at mem + 12 as an integer pointer and then dereference it (interpreting its bytes as an integer) which prints 8
-    printf("%d\n", *(int*)(mem + 12));
-
-    return 0;
+int* fn4() {
+    int* x = malloc(sizeof(int));
+    *x = 5;
+    return x;
 }
+
+int main() { return 0; }
